ffba18a3c058dcedc031bad193475b2eebdca1bd
NOlR <apple@appledeMacBook-Air.local>
2020-10-21 23:00:48 +0800

    代码打卡第二天

--- src/main/java/com/javacode/day01/BubbleSort.java
+++ src/main/java/com/javacode/day01/BubbleSort.java
@@ -2,16 +2,24 @@ package com.javacode.day01;
 
 /**
  * @ClassName BubbleSort
- * @Description TODO
+ * @Description 冒泡排序的实现
  * @Author apple
  * @Date 2020/10/20
  **/
+/**
+ *
+ * @param a 数组
+ * @param n 排序趟数
+ * */
 public class BubbleSort {
     public static void bubbleSort(int[] a, int n) {
         int i, j;
+        //表示n次排序的过程
         for (i = 0; i < n; i++) {
             for (j = 1; j < n - i; j++) {
+                //前面的数字大于后面的数字就交换
                 if (a[j - 1] > a[j]) {
+                    //交换a[j-1]和a[j]
                     int temp;
                     temp = a[j - 1];
                     a[j - 1] = a[j];
