a41430ca235ba80f40f8173ab262c299cf3d0d81
NOlR <apple@appledeMacBook-Air.local>
2020-10-24 23:14:03 +0800

    代码打卡第五天

---
+++ src/main/java/com/javacode/day05/BaseGraph.java
@@ -0,0 +1,16 @@
+package com.javacode.day05;
+
+/**
+ * @ClassName BaseGraph
+ * @Description 抽象基类：BaseGraph，抽象类一般用Abstract或Base开头
+ * @Author apple
+ * @Date 2020/10/24
+ **/
+abstract class BaseGraph {
+    /**
+     * 求图形的周长
+     *
+     * @return  图形的周长
+     */
+    public abstract double getCircumference();
+}
